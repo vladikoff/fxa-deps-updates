8f65c714f4437367f9764cb45c825d316a7c6152
Vlad Filippov <vlad.filippov@gmail.com>
2017-03-22 20:54:35 -0400

    fix(repos): remove auth-mailer, it was deprecated

--- repos.sh
+++ repos.sh
@@ -5,7 +5,6 @@ cd projects
 git clone https://github.com/mozilla/fxa-content-server.git &
 git clone https://github.com/mozilla/fxa-auth-server.git &
 git clone https://github.com/mozilla/fxa-auth-db-mysql.git &
-git clone https://github.com/mozilla/fxa-auth-mailer.git &
 git clone https://github.com/mozilla/fxa-customs-server.git &
 git clone https://github.com/vladikoff/browserid-verifier.git -b http &
 git clone https://github.com/mozilla/fxa-oauth-server.git &
